125586cf5f923e99d781a47b8e906942703d4170
Matheus Luan Campos de Carvalho <matheus.carvalho@decisaosistemas.com.br>
2024-04-01 16:23:38 -0300

    Criando rota de criação de pagador

---
+++ backend/src/application/entryPoint/pagador/criarPagadorEntrypoint.ts
@@ -0,0 +1,16 @@
+import EntryPoint from '../../../domain/implementations/entity/entryPoint/EntryPoint';
+import IController from '../../../domain/protocols/services/Controller';
+import { IEntrypointGuard } from '../../../domain/protocols/services/EntrypointGuard';
+import { HttpMetodos } from '../../../infra/express/models/httpMetodoEnum';
+
+export default class CriarPagadorEntrypoint {
+  public path: string = '/criar-pagador';
+
+  public httpMetodo: HttpMetodos = HttpMetodos.post;
+
+  public controller: IController;
+
+  constructor(pController: IController) {
+    this.controller = pController;
+  }
+}
