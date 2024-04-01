7fe325208ac094d2a1a28a3892e20b900e66361d
Matheus Luan Campos de Carvalho <matheus.carvalho@decisaosistemas.com.br>
2024-04-01 16:15:03 -0300

    Criando criarPagadorController

---
+++ backend/src/application/controllers/pagador/criarPagadorController.ts
@@ -0,0 +1,34 @@
+import UnitOfWork from '../../../domain/implementations/entity/UnitOfWork';
+import EntrypointData from '../../../domain/implementations/entity/entryPoint/EntryPointData';
+import EntryPointResponse from '../../../domain/implementations/entity/entryPoint/EntryPointResponse';
+import EntryPointSuccess from '../../../domain/implementations/entity/entryPoint/EntryPointSuccess';
+import { CriarPagador } from '../../../domain/implementations/usecase/pagador/criarPagador/CriarPagador';
+import { CriarPagadorInput } from '../../../domain/implementations/usecase/pagador/criarPagador/CriarPagadorInput';
+import IController from '../../../domain/protocols/services/Controller';
+
+
+export class CriarPagadorController implements IController {
+  constructor(private useCase: CriarPagador) { }
+
+  public async execute(pData: EntrypointData): Promise<EntryPointSuccess> {
+    const unitOfWork = new UnitOfWork(pData.tokenAuthorization);
+    try {
+      const inputPagador = new CriarPagadorInput({
+        body: pData.body,
+        parametros: pData.parametros,
+        tokenAuthorization: pData.tokenAuthorization,
+      });
+      await unitOfWork.init();
+      const result = await this.useCase.execute(unitOfWork, inputPagador);
+      await unitOfWork.commit();
+      if (result) {
+        return new EntryPointSuccess('Pagador criado com sucesso.', result);
+      }
+      return new EntryPointResponse(false, 400, 'Pagador já cadastrado!', result, null);
+    } catch (error) {
+      await unitOfWork.rollBack();
+      return Promise.reject(error);
+    }
+  }
+}
+
