d49a88a07c18f7673abbaa6b06e189209cdf4dc2
Matheus Luan Campos de Carvalho <matheus.carvalho@decisaosistemas.com.br>
2024-04-11 14:45:33 -0300

    Ajustando Login

--- backend/src/domain/implementations/usecase/conta/login/LoginOutput.ts
+++ backend/src/domain/implementations/usecase/conta/login/LoginOutput.ts
@@ -1,9 +1,11 @@
 import { Conta } from '../../../entity/objectValues/Conta';
 
 export class LoginOutput {
+  public idConta: string;
   public nome: string;
   public email: string;
   constructor(pConta: Conta) {
+    this.idConta = pConta.idConta;
     this.nome = pConta.nome;
     this.email = pConta.email;
   }
