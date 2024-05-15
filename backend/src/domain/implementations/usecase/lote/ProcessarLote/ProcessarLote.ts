fa2d7eaab17e7a60802d5dae259a413bc8227dcf
Matheus Luan Campos de Carvalho <matheus.carvalho@decisaosistemas.com.br>
2024-05-15 14:53:50 -0300

    Refatoração Processar Lote

--- backend/src/domain/implementations/usecase/lote/ProcessarLote/ProcessarLote.ts
+++ backend/src/domain/implementations/usecase/lote/ProcessarLote/ProcessarLote.ts
@@ -4,12 +4,13 @@ import ILoteRepository from '../../../../protocols/repository/loteRepository';
 import { ProcessarLoteInput } from './ProcessarLoteInput';
 import InformacaoNaoEncontrada from '../../../entity/errors/InfomacaoNaoEncontrada';
 import AcaoInvalida from '../../../entity/errors/AcaoInvalida';
-import { GerarData } from '../../../services/gerarData';
+import { GerarData, GerarNovaData } from '../../../services/gerarData';
 import { Movimentacao } from '../../../entity/objectValues/Movimentacao';
 import { v4 } from 'uuid';
 import IMovimentacaoRepository from '../../../../protocols/repository/movimentacaoRepository';
 import ILancamentoRepository from '../../../../protocols/repository/lancamentoRepository';
 import { Lancamento } from '../../../entity/objectValues/Lancamento';
+import { Titulo } from '../../../entity/objectValues/Titulo';
 
 export class ProcessarLote {
   constructor(
@@ -23,7 +24,7 @@ export class ProcessarLote {
   public async execute(pUnitOfWork: UnitOfWork, pInputLote: ProcessarLoteInput): Promise<boolean> {
     const isLoteExist = await this.loteRepository.buscaLotePorId(pUnitOfWork, pInputLote.idLote);
 
-    if(!isLoteExist) {
+    if(!!isLoteExist === false) {
       throw new InformacaoNaoEncontrada('Lote não encontrado');
     }
       
@@ -31,37 +32,43 @@ export class ProcessarLote {
       throw new AcaoInvalida('Lote já foi processado');
     }
 
-    // if(isLoteExist.situacao === 'PROCESSADO') {
-    //   throw new AcaoInvalida('Lote já foi processado');
-    // }
-
-    const listagemDeTitulos = await this.tituloRepository.listarTitulosPorLote(pUnitOfWork, pInputLote.idLote, pInputLote.idConta);
-    let aux = 0;
-
-    for(let i of listagemDeTitulos) {
-      if(aux === Number(i.numeroTitulo)) {
-        throw new AcaoInvalida('Não pode processar Lote pois existem Títulos iguais');
-      }
-      aux = Number(i.numeroTitulo);
+    if(isLoteExist.situacao === 'PROCESSADO') {
+      throw new AcaoInvalida('Lote já foi processado');
     }
 
-    for(let i of listagemDeTitulos) {
+    const listagemDeTitulos: Titulo[] = await this.tituloRepository.listarTitulosPorLote(pUnitOfWork, pInputLote.idLote, pInputLote.idConta);
+    
+    let numeroDoTituloTemporario = 0;
+
+    for(let titulo of listagemDeTitulos) {
+      if(numeroDoTituloTemporario === Number(titulo.numeroTitulo)) {
+        throw new AcaoInvalida('Não pode processar Lote pois existem Títulos iguais');
+      }
+      numeroDoTituloTemporario = Number(titulo.numeroTitulo);
+    }
+
+    for(let titulo of listagemDeTitulos) {
       let movimentacao = new Movimentacao({
         idMovimentacao: v4(),
-        saldo: i.valorDoTitulo,
-        valorTotalPrincipal: i.valorDoTitulo,
+        saldo: titulo.valorDoTitulo,
+        valorTotalPrincipal: titulo.valorDoTitulo,
         valorTotalMulta: 0,
         valorTotalJuros: 0,
         valorTotalDesconto: 0,
-        idTitulo: i.idTitulo,
-        idConta: i.idConta,
+        idTitulo: titulo.idTitulo,
+        idConta: titulo.idConta,
       });
 
       await this.movimentacaoRepository.criar(pUnitOfWork, movimentacao);
     }
 
-    const data = GerarData(new Date());
-    await this.loteRepository.editarLoteParaProcessado(pUnitOfWork, pInputLote.idLote, pInputLote.idConta, data);
+    const dataDeHoje: Date = new GerarNovaData().zerarHoras();
+
+    // dataDeHoje.zerarHoras();
+    // const data = GerarData(new Date());
+    // const data = dataDeHoje.zerarHoras();
+
+    await this.loteRepository.editarLoteParaProcessado(pUnitOfWork, pInputLote.idLote, pInputLote.idConta, dataDeHoje);
     await this.tituloRepository.editarSituacaoTitulos(pUnitOfWork, pInputLote.idLote, pInputLote.idConta);
 
     return Promise.resolve(true);
--- backend/src/domain/protocols/repository/loteRepository.ts
+++ backend/src/domain/protocols/repository/loteRepository.ts
@@ -6,7 +6,9 @@ export default interface ILoteRepository {
   criar(pUnitOfWork: UnitOfWork, pLote: Lote): Promise<Lote>;
   buscaLotePorId(pUnitOfWork: UnitOfWork, pIdLote: string): Promise<Lote | null>;
   editarValorTotalDeTitulosPorLote(pUnitOfWork: UnitOfWork, pIdLote: string, pValor: number, pQtd: number): Promise<boolean>;
+  
   editarLoteParaProcessado(pUnitOfWork: UnitOfWork, pIdLote: string, pIdConta: string, pDate: Date): Promise<boolean>;
+  
   listarLotes(pUnitOfWork: UnitOfWork, pIdConta: string): Promise<Lote[]>;
   filtrarLotes(pUnitOfWork: UnitOfWork, pDataInicial: Date, pDataFinal: Date, pSituacao: string, pIdConta: string): Promise<Lote[]>
   filtrarTodosLotes(pUnitOfWork: UnitOfWork, pDataInicial: Date, pDataFinal: Date, pIdConta: string): Promise<Lote[]>
